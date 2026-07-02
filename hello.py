7f4fd65b0cbb51ed3f653d63c2e754f47f15f929
Olanrewaju Adebayo <laanreadebayo@gmail.com>
2026-07-02 18:23:30 +0100

    Refactor hello.py to simplify the Hello World program

--- hello.py
+++ hello.py
@@ -1,7 +1,2 @@
-def main():
-    print("Hello, World!")
-    print("My name is Lanre.")
-    print("Welcome to my CSE 310 Software Portfolio!")
-
-if __name__ == "__main__":
-    main()
+# Hello World program in Python
+print("Hello World!")
