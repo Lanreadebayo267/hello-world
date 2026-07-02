4f5eac36b4aef8f23babc9de3315ee2af5132c71
Olanrewaju Adebayo <laanreadebayo@gmail.com>
2026-07-02 18:15:11 +0100

    Fix typo in welcome message in hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def main():
     print("Hello, World!")
     print("My name is Lanre.")
-    print("Welcome to my CSE 310 Software Portolio!")
+    print("Welcome to my CSE 310 Software Portfolio!")
 
 if __name__ == "__main__":
     main()
